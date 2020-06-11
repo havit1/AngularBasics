c3b8b18744df35b153a8ed3c785a9fb76922a75b
Havit01 <sashaka2013@gmail.com>
2020-06-11 17:05:41 +0300

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-basics';
+  toggle = true
+
+  cards: Card[] = [ 
+    {
+       text: 'This is card 1',
+       title: 'Card 1'
+    },
+    {
+       text: 'This is card 2',
+       title: 'Card 2'
+    },
+    {
+       text: 'This is card 3',
+       title: 'Card 3'
+    },
+  ]
+
+  toggleCards() {
+    this.toggle = !this.toggle
+  }
 }
